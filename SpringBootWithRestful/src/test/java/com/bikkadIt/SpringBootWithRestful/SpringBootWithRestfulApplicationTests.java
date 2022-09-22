46106d0e5ff256e58a0fc416fc8ea65a4b33ecce
NikitaDevkar <nikitadevkar387@gmail.com>
2022-09-22 13:51:10 +0530

    project added to central repository

---
+++ SpringBootWithRestful/src/test/java/com/bikkadIt/SpringBootWithRestful/SpringBootWithRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIt.SpringBootWithRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
